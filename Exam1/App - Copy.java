53fab76539c6187c6ef7a96f6a62d249599abb18
Arjay C. Balboa <111472877+Chopyzx@users.noreply.github.com>
2022-09-05 10:02:26 +0800

    Add files via upload

---
+++ Exam1/App - Copy.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Character Class
+        Student Arjay = new Student();
+
+        Arjay.surname = "Balboa";
+        Arjay.firstname = "Arjay";
+        Arjay.middleInitial = 'C';
+        Arjay.dateOfBirth = "09/08/2003";
+        Arjay.studentNumber = 2022102385;
+        Arjay.studentEmailAddress = "balboaac@students.national-u.edu.ph";
+        Arjay.IAmAwesome = true;
+        Arjay.sayMystudentNumber();
+        Arjay.sayMytudentEmailAddress();
+        Arjay.sayMytudentiAmAwesome();
+
+
+    }
+}
---
+++ Exam1/Student - Copy.java
@@ -0,0 +1,35 @@
+public class Student {
+    // Character attributes
+    String surname;
+    String firstname;
+    char middleInitial;
+    String dateOfBirth;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean IAmAwesome;
+    
+
+    // Character method
+    public void sayMysurname() {
+        System.out.println("My Surname is " + surname);
+    }
+    public void sayMyfirstname() {
+        System.out.println("My firstname is " + firstname);
+    }
+    public void sayMymiddleInitial() {
+        System.out.println("My middle initial is " + middleInitial);
+    }
+    public void sayMydateOfBirth() {
+        System.out.println("My birthday is " + dateOfBirth);
+    }
+    public void sayMystudentNumber() {
+        System.out.println("My ST number is " + studentNumber);
+    }
+    public void sayMytudentEmailAddress() {
+        System.out.println("My email is " + studentEmailAddress);
+    }
+    public void sayMytudentiAmAwesome() {
+        System.out.println("I Am Awesome " + IAmAwesome);
+    }
+}
+   
